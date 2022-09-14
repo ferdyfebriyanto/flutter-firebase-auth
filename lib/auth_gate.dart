d46e660e29708e9a9cf752cf859a3dde3fb76e3a
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-09-14 23:27:39 +0700

    Add Google sign-in button

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -16,13 +16,14 @@ class AuthGate extends StatelessWidget {
           return SignInScreen(
             providerConfigs: const [
               EmailProviderConfiguration(),
+              GoogleProviderConfiguration(),
             ],
             headerBuilder: (context, constraints, shrinkOffset) {
               return Padding(
                 padding: const EdgeInsets.all(20),
                 child: AspectRatio(
                   aspectRatio: 1,
-                  child: Image.asset('assets/flutterfire_300x.png'),
+                  child: Image.asset('flutterfire_300x.png'),
                 ),
               );
             },
@@ -48,12 +49,13 @@ class AuthGate extends StatelessWidget {
                 padding: const EdgeInsets.all(20),
                 child: AspectRatio(
                   aspectRatio: 1,
-                  child: Image.asset('assets/flutterfire_300x.png'),
+                  child: Image.asset('flutterfire_300x.png'),
                 ),
               );
             },
           );
         }
+
         return const HomeScreen();
       },
     );
